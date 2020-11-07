2a667f02ca5498a2e916bd18a76d5cbddaa8ec26
Nicolas Vera <57852440+queondatodotranqui@users.noreply.github.com>
2020-11-07 00:42:13 -0300

    Cambia colores
    Cambia colores segun seleccion

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 
-window.addEventListener('click', (e)=>{
 
+var colores = document.getElementById('colores');
+var colorValor = colores.value;
+
+colores.addEventListener('input', ()=>{
+    colorValor = colores.value;
+})
+
+function colorear(e, color){
     var caja = e.target;
 
-    //console.log(caja)
+    //console.log(colorValor)
 
     if (caja.classList == 'box'){
-        caja.style.backgroundColor = 'blue';
+        caja.style.backgroundColor = color;
     }
-})
+}
+
+window.addEventListener('click', (e)=>{
+    colorear(e, colorValor);
+});
